9afce5bc9f604a7ae668c3548ca158a9b10c6f4b
Adrian Monsalve <adrianmonsalve02@gmail.com>
2025-05-28 21:28:19 -0500

    ajustando y creando componentes nuevos

--- Frontend/eventos-frontend/src/app/features/auth/cambiar-password/cambiar-password.component.ts
+++ Frontend/eventos-frontend/src/app/features/auth/cambiar-password/cambiar-password.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './cambiar-password.component.css'
 })
 export class CambiarPasswordComponent {
+  cambiarPassword = 'hola';
+  constructor() {
+    // Aquí puedes inicializar cualquier lógica necesaria
+  }
 
+  onSubmit() {
+    // Lógica para cambiar la contraseña
+    console.log('Contraseña cambiada:', this.cambiarPassword);
+    // Aquí podrías llamar a un servicio para actualizar la contraseña en el backend
+  }
+
+  cancelar() {
+    // Lógica para cancelar el cambio de contraseña
+    console.log('Cambio de contraseña cancelado');
+    // Aquí podrías redirigir al usuario a otra página o limpiar el formulario
+  }
 }
